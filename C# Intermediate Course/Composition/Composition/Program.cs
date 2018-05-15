22654803738d6d8554327ece5887914fa6e9ed6c
Tejas Shroff <tejasshroff24@gmail.com>
2018-05-15 17:29:49 -0400

    Adding composition section

--- C# Intermediate Course/Composition/Composition/Program.cs
+++ C# Intermediate Course/Composition/Composition/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Composition
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
         }
     }
 }
