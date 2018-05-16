072967bee9830ce3c35804f7db0de2b0f37916fc
Tejas Shroff <tejasshroff24@gmail.com>
2018-05-16 10:51:58 -0400

    Completed exercise for inheritence section

---
+++ C# Intermediate Course/Inheritance-Exercises/Inheritance-Exercises/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercises
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+
+        }
+    }
+}
---
+++ C# Intermediate Course/Inheritance-Exercises/Inheritance-Exercises/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Inheritance_Exercises
+{
+    class Stack
+    {
+        private readonly List<object> _list;
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                _list.Add(obj);
+                Console.WriteLine("{0} has been added to the list", obj);
+            }
+            else
+                throw new InvalidOperationException("Cannot stored object in list");
+        }
+
+        public object Pop()
+        {
+            var lastInList = _list[_list.Count - 1];
+
+            if (_list.Count > 0)
+                _list.RemoveAt(0);
+            else
+                throw new InvalidOperationException("Cannot pop any more objects from the list");
+
+            return lastInList;
+
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+            Console.WriteLine("The stack has been cleared");
+        }
+    }
+}
