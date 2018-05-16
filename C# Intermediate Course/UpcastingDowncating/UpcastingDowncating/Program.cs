cac532c1e1736a5df8e694e2e44a0c45180fcab0
Tejas Shroff <tejasshroff24@gmail.com>
2018-05-16 10:09:31 -0400

    Completed upcasting/downcasting section

---
+++ C# Intermediate Course/UpcastingDowncating/UpcastingDowncating/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingDowncating
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Downcasting
+            Shape shape = new Text();
+            Text text = (Text)shape;
+            text.FontName = "Google Font Name";
+            */
+
+
+            /* Upcasting
+            StreamReader reader = new StreamReader(new MemoryStream());
+
+            var list = new ArrayList();
+            list.Add("we can pass any object here");
+
+            var anotherList = new List<Text>();
+
+
+            Text text = new Text();
+            Shape shape = text;
+
+            // Both text and shape are referneces to the same object in memory
+            text.Width = 200; 
+            shape.Width = 100;
+
+            Console.WriteLine(text.Width);
+            */
+            
+
+        }
+    }
+}
---
+++ C# Intermediate Course/UpcastingDowncating/UpcastingDowncating/Text.cs
@@ -0,0 +1,9 @@
+﻿namespace UpcastingDowncating
+{
+    public class Text: Shape
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+    }
+}
