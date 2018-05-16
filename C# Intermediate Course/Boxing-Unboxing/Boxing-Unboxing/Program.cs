75a959df0bc6390b8e3093638f85ae98c1bd3732
Tejas Shroff <tejasshroff24@gmail.com>
2018-05-16 10:24:56 -0400

    Completed the boxing/unboxing section

---
+++ C# Intermediate Course/Boxing-Unboxing/Boxing-Unboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList(); // boxing happens becuase values are casted into objects
+            list.Add(1);
+            list.Add("Tejas");
+
+            var anotherList = new List<int>(); // no boxing + type safety creates better software
+        }
+    }
+}
