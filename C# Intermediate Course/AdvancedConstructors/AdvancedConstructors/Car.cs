8da5fc042ba66579aa8df2250588e7abb32465ba
Tejas Shroff <tejasshroff24@gmail.com>
2018-05-16 09:32:04 -0400

    Completed advanced constructors section

---
+++ C# Intermediate Course/AdvancedConstructors/AdvancedConstructors/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdvancedConstructors
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initizlized. {0}", registrationNumber);
+        }
+    }
+}
