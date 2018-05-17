512a3d650a1dd341ed938811f517b51e124483c3
Tejas Shroff <tejasshroff24@gmail.com>
2018-05-17 10:34:28 -0400

    Completed method overriding section

---
+++ C# Intermediate Course/MethodOverriding/MethodOverriding/Shape.cs
@@ -0,0 +1,37 @@
+﻿namespace MethodOverriding
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public virtual void Draw()
+        {
+            // Default implementation
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            System.Console.WriteLine("Draw a circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            System.Console.WriteLine("Draw a rectangle");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            System.Console.WriteLine("Draw a triangle");
+        }
+    }
+}
