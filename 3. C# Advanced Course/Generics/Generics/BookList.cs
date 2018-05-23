fac7f5aa2a94927b3b78e1b4836d468acd3ccfc1
Tejas Shroff <tejasshroff24@gmail.com>
2018-05-23 17:25:11 -0400

    added section on Generics in C#
    Also named naming conventions of subfolders

---
+++ 3. C# Advanced Course/Generics/Generics/BookList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Generics
+{
+    public class BookList
+    {
+        public void Add(Book book)
+        {
+            throw new NotFiniteNumberException();
+        }
+
+        public Book this[int index]
+        {
+            get { throw new NotFiniteNumberException();  }
+        }
+    }
+
+    public class ObjectList
+    {
+        public void Add(object value)
+        {
+
+        }
+
+        public object this[int index]
+        {
+            get { throw new NotFiniteNumberException();  }
+        }
+    }
+}
